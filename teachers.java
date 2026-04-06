7d3641de287e2adf74230e9fef5ff48864c29014
Anuprasad <anuprasad324133@gmail.com>
2026-04-06 14:39:17 +0530

    Implement Teacher class extending Employee

---
+++ teachers.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+class Employee{
+        int empid;
+        String name;
+        double salary;
+        String address;
+
+        Employee(int empid,String name,double salary,String address){
+                this.empid=empid;
+                this.name=name;
+                this.salary=salary;
+                this.address=address;
+        }
+}
+
+class Teacher extends Employee{
+        String department;
+        String subject;
+
+        Teacher(int empid,String name,double salary,String address,String department,String subject){
+                super(empid,name,salary,address);
+                this.department=department;
+                this.subject=subject;
+        }
+
+        void display(){
+                System.out.println("Employee ID:" + empid);
+                System.out.println("Name:" + name);
+                System.out.println("Salary:" + salary);
+                System.out.println("Address:" + salary);
+                System.out.println("Department:" + department);
+                System.out.println("Subject:" + subject);
+                System.out.println("----------------------");
+        }
+}
+
+public class teachers{
+        public static void main(String[] args){
+                Scanner sc=new Scanner(System.in);
+
+                System.out.print("Enter number of teachers:");
+                int n =sc.nextInt();
+                sc.nextLine();
+
+                Teacher t[]=new Teacher[n];
+
+                for (int i=0;i<n;i++){
+                        System.out.println("Enter details of teacher" +(i+1));
+                        System.out.print("Employee ID:");
+                        int id=sc.nextInt();
+                        sc.nextLine();
+
+                        System.out.print("Name:");
+                        String name=sc.nextLine();
+
+                        System.out.print("Salary:");
+                        double salary=sc.nextDouble();
+                        sc.nextLine();
+
+                        System.out.print("Address:");
+                        String address=sc.nextLine();
+
+                        System.out.print("Department:");
+                        String dept=sc.nextLine();
+
+                        System.out.print("Subject:");
+                        String subject=sc.nextLine();
+
+                        t[i]=new Teacher(id,name,salary,address,dept,subject);
+                }
+                System.out.println("\nTeachers Details:");
+                for(int i=0;i<n;i++){
+                        t[i].display();
+                }
+        }
+}
